b01881d0b413a3bbbee307c8561b5fae1b868013
Doug Lau <doug.p.lau@gmail.com>
2019-07-19 19:11:32 -0500

    Added additional round-trip test.

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -1,6 +1,6 @@
 use serde_derive::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 struct A {
     name: String,
     age: u8,
@@ -8,16 +8,23 @@ struct A {
 }
 
 #[test]
-fn round_trip() -> muon::Result<()> {
+fn derived_ser_de() -> muon::Result<()> {
     let a = A {
         name: "First, Last".to_string(),
         age: 21,
         debt: -5_000_000,
     };
-
     let s = muon::to_string(&a)?;
     let aa: A = muon::from_str(&s)?;
+    assert_eq!(a, aa);
+    Ok(())
+}
 
-    assert!(a == aa);
+#[test]
+fn derived_de_ser() -> muon::Result<()> {
+    let s = "name: Me, Myself\nage: 99\ndebt: -2\n";
+    let a: A = muon::from_str(&s)?;
+    let ss = muon::to_string(&a)?;
+    assert_eq!(s, ss);
     Ok(())
 }
