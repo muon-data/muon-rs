cc5092dcc7c8dbf2edba8a50fa01d59c0ac9c033
Doug Lau <doug.p.lau@gmail.com>
2019-08-13 18:45:17 -0500

    Added isize / usize support to Serializer.

--- src/ser.rs
+++ src/ser.rs
@@ -35,7 +35,7 @@ macro_rules! impl_item {
     };
 }
 
-impl_item!(i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char);
+impl_item!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize f32 f64 char);
 
 impl Item for &str {
     fn write<W: Write>(&self, writer: &mut W) -> Result<()> {
